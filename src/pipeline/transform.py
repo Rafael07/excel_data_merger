7335ed507d141b9345125f8529c64c989ca489ed
Rafael07 <rafaelrodriguesdsa@gmail.com>
2025-04-16 16:23:51 -0300

    Codigo transform

--- src/pipeline/transform.py
+++ src/pipeline/transform.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from typing import List
 
-"""
+
+
+def concat_dataframes(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
+
+    """
     Função para transformar uma lista de DataFrames em um DataFrame
 
     args: data_frame_list (List[pd.DataFrame]): lista de DataFrames
     return: DataFrame
-"""
+    """
 
-def concat_dataframes(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
     return pd.concat(data_frame_list, ignore_index=True)
